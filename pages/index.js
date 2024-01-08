132d93ba24536ec85d351e8b9b9b35ef24f71fc6
Erick Leandro <j.erickleandro@gmail.com>
2024-01-08 14:16:02 +0000

    chore: add init setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <h1>Página em construção</h1>
+      <p>Desculpe pelo inconveniente. Estamos trabalhando para melhorar o site.</p>
+    </div>
+  );
+};
+
+export default index;
